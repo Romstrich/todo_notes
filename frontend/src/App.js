1b07551f0d026806d8a36d58de251b656fb30219
Roman <roman.motrich@yandex.ru>
2022-03-21 02:54:06 +0300

    Main App +

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            'authors':[]
+        }
+    }
+
+
+    render(){
+        return (
+            <div>
+                Main App
+            </div>
+        );
+    }
+}
+
+export default App;
