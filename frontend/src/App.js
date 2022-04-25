e4890986e64c784677036e70c015b7f63714a1a0
Roman <roman.motrich@yandex.ru>
2022-04-25 18:14:25 +0300

    this->set token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,10 +21,14 @@ class App extends React.Component{
         }
     }
 
+    set_token(token){
+    }
+
     get_token(username,password){
         axios.post("http://127.0.0.1:8000/api-token-auth/",
         {'username':username,password:password})
-        .then(response=>{console.log(response.data['token'])}).catch(error => console.log(error))
+        .then(response=>{this.set_token(response.data['token'])})
+        .catch(error => console.log(error))
 
     }
 
