49ee48885af05bce588b9cc8035628185e13645b
Roman <roman.motrich@yandex.ru>
2022-04-25 18:30:03 +0300

    is auth?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
-import UserList from "./components/Users.js"
-import ProjectList from "./components/Projects.js"
-import TodoList from "./components/Todo_notes.js"
-import LoginForm from "./components/Auth.js"
-import {HashRouter, Route, BrowserRouter,Link, Switch,Redirect} from "react-router-dom"
-
+import UserList from "./components/Users.js";
+import ProjectList from "./components/Projects.js";
+import TodoList from "./components/Todo_notes.js";
+import LoginForm from "./components/Auth.js";
+import {HashRouter, Route, BrowserRouter,Link, Switch,Redirect} from "react-router-dom";
+import Cookies from "universal-cookie";
 
 
 class App extends React.Component{
@@ -21,23 +21,8 @@ class App extends React.Component{
         }
     }
 
-    set_token(token){
-    }
-
-    get_token(username,password){
-        axios.post("http://127.0.0.1:8000/api-token-auth/",
-        {'username':username,password:password})
-        .then(response=>{this.set_token(response.data['token'])})
-        .catch(error => console.log(error))
-
-    }
-
-    logout(){}
-
-
-    componentDidMount(){
-
-        axios.get("http://127.0.0.1:8000/api/users/").then(response=>{
+    load_data(){
+    axios.get("http://127.0.0.1:8000/api/users/").then(response=>{
         this.setState(
             {'users':response.data}
         )}).catch(error => console.log(error))
@@ -51,6 +36,43 @@ class App extends React.Component{
         this.setState(
             {'todo_notes':response.data}
         )}).catch(error => console.log(error))
+    }
+
+
+    set_token(token){
+//    console.log(token)
+    const cookies=new Cookies()
+    cookies.set('token',token)
+    this.setState({'token':token},()=>this.load_data())
+    }
+
+    get_token(username,password){
+        axios.post("http://127.0.0.1:8000/api-token-auth/",
+        {'username':username,password:password})
+        .then(response=>{this.set_token(response.data['token'])})
+        .catch(error => console.log(error))
+
+    }
+
+    is_auth(){
+        return !!this.state.token
+    }
+
+    get_headers(){
+        let headers={
+        'Content-type':'application/json'
+        }
+        if(this.is_auth()){
+            headers['Authorisation']='Token ${this.state.token}'
+            }
+    }
+
+    logout(){}
+
+
+    componentDidMount(){
+
+
     }
 
     render(){
