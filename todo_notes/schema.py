b33ada75ea4e2d30b9c7364b682c5b2b57961d26
Roman <roman.motrich@yandex.ru>
2022-05-05 06:58:22 +0300

    todo_notes works
    переходим к подключению дополнительных моделей к GraphQL

--- todo_notes/schema.py
+++ todo_notes/schema.py
@@ -1,12 +1,30 @@
-#import graphene
-from graphene import ObjectType, Schema, String
+# import graphene
+from graphene import ObjectType, Schema, String, List
 from graphene_django import DjangoObjectType
-from todo.models import Todo,Project
+from todo.models import Todo, Project
 from users.models import User
 
 
+# class Querry(ObjectType):
+#     hello=String(default_value='HI, MAN!')
 
-class Querry(ObjectType):
-    hello=String(default_value='HI, MAN!')
+class TodoType(DjangoObjectType):
+    class Meta:
+        model = Todo
+        fields = '__all__'
 
-schema=Schema(query=Querry)
+class ProjectType(DjangoObjectType):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class Query(ObjectType):
+
+    all_todo=List(TodoType)
+
+    def resolve_all_todo(root,info):
+        return Todo.objects.all()
+
+
+schema = Schema(query=Query)
